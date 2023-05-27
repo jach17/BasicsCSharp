f45dd49890338bf28a2d3a4fade4aa24b7fdc4f5
Jonathan Dev <jonathan.aldana170500@gmail.com>
2023-05-27 00:38:07 -0600

    Init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace holacsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
